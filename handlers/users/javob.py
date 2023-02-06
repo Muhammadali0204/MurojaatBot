d68f33a2c5d9b20652d1fa7f4e507e94748c8270
Muhammadali0204 <mamadjonovmuhammadali7@gmail.com>
2023-02-06 11:58:54 +0500

    xabar jo'natish

--- handlers/users/javob.py
+++ handlers/users/javob.py
@@ -6,6 +6,6 @@ from loader import dp, bot
 
 @dp.message_handler()
 async def javob(message : types.message):
-    await message.answer("<b>Xabaringiz qabul qilinddi, tez orada javob qaytaramiz!</b>")
+    await message.answer("<b>Xabaringiz qabul qilindi</b>✅\n\n<i>Tez orada javob beriladi!</i>")
     for admin in ADMINS:
         await bot.send_message(admin, f"<b>Foydalanuvchi : {message.from_user.full_name} xabar yubordi</b>")
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -10,4 +10,4 @@ async def bot_start(message: types.Message):
     await message.answer(f"<b>Assalomu alaykum {message.from_user.get_mention(message.from_user.full_name)}.</b>")
     await message.answer(f"<b>Savolingiz yoki taklifingiz bo`lsa yozib qoldiring</b>\n\n<i>Tez orada javob beriladi!</i>")
     for admin in ADMINS:
-        await bot.send_message(admin, f"<b>Foydalanuvchi : {message.from_user.full_name} xabar yubordi</b>")
+        await bot.send_message(admin, f"<b>Foydalanuvchi : {message.from_user.full_name} /start bosdi</b>")
